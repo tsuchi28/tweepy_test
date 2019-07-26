11b37052dc2ee26fc15a2278e342ad28ff5d1a2e
tsuchi28 <52030626+tsuchi28@users.noreply.github.com>
2019-07-26 19:00:54 +0900

    Add files via upload

---
+++ tauth.py
@@ -0,0 +1,16 @@
+﻿#coding:UTF-8
+import tweepy
+
+CONSUMER_KEY = ""          #入力
+CONSUMER_SECRET = ""       #入力
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+
+ACCESS_TOKEN = ""          #入力
+ACCESS_TOKEN_SECRET = ""   #入力
+
+print("auth")
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+print("Done!")
